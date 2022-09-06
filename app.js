6b76ef4d70af563d7d21361d38612c3940313676
Felix Mendez <mendez.felixrn@gmail.com>
2022-09-06 19:41:19 -0400

    Change create square function so it takes an input about how many squares to create and modifies the ammount of rows and columns

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const grid = document.getElementById("grid");
-grid.style.gridTemplateColumns = "repeat(16, 1fr)";
-grid.style.gridTemplateRows = "repeat(16, 1fr)"
-
- const createSquares = () => {
-    for (let i =0; i < 256; i++){
+ const createSquares = (size = 16) => {
+    // first adjust the ammount of squares the board then 
+    const grid = document.getElementById("grid");
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    for (let i =0; i < size*size; i++){
         let square = document.createElement("div");
         square.setAttribute("class", "grid-square");
         square.style.backgroundColor = "black";
@@ -11,9 +11,8 @@ grid.style.gridTemplateRows = "repeat(16, 1fr)"
         grid.insertAdjacentElement("beforeend", square);
     }
  };
-
- createSquares();
-
+createSquares(30)
+createSquares(90)
 
  const squares = document.querySelectorAll(".grid-square")
 squares.forEach(square => {
